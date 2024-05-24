b9dfe2077cdb8f5d32c05211931ee57a8d367484
rabby-web <rabby.webdeveloper@gmail.com>
2024-05-24 16:14:22 +0600

    product interface and model create

--- src/modules/product/product.interface.ts
+++ src/modules/product/product.interface.ts
@@ -1,20 +1,22 @@
+// variant type
 export type TVariant = {
-    type: string;
-    value: string;
-  };
-  
-  type TInventory = {
-    quantity: number;
-    inStock: boolean;
-  };
-  
-  type TProduct = {
-    name: string;
-    description: string;
-    price: number;
-    category: string;
-    tags: string[];
-    variants: TVariant[];
-    inventory: TInventory;
-  };
+  type: string;
+  value: string;
+};
 
+// inventory type
+export type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+// product type
+export type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: TVariant[];
+  inventory: TInventory;
+};
---
+++ src/modules/product/product.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+import { TInventory, TProduct, TVariant } from "./product.interface";
+
+// variant schema
+const variantSchema = new Schema<TVariant>({
+  type: { type: String },
+  value: { type: String },
+});
+
+// inventory schema
+const inventorySchema = new Schema<TInventory>({
+  quantity: { type: Number },
+  inStock: { type: Boolean },
+});
+
+// product schema
+const productSchema = new Schema<TProduct>({
+  name: { type: String },
+  description: { type: String },
+  price: { type: Number },
+  category: { type: String },
+  tags: { type: [String] },
+  variants: { type: [variantSchema] },
+  inventory: { type: inventorySchema },
+});
+
+// created a product model and export
+export const Products = model<TProduct>("Products", productSchema);
